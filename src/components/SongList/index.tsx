fb5c51588907583fd4016ed0d71ab31bffffd93e
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-19 23:08:50 -0300

    create SongList component

---
+++ src/components/SongList/index.tsx
@@ -0,0 +1,22 @@
+import { Box, Divider } from '@chakra-ui/layout'
+import SongItem from 'components/SongItem'
+
+export type SongListProps = {
+  songs?: SongType[]
+}
+
+const SongList = ({ songs }: SongListProps) => {
+  return (
+    <>
+      {songs?.map((song: SongType, index: number) => (
+        <Box key={song.id} my="10px">
+          <SongItem song={song} />
+
+          {index < songs!.length - 1 && <Divider />}
+        </Box>
+      ))}
+    </>
+  )
+}
+
+export default SongList
