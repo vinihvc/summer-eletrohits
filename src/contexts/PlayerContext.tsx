447ce4f1e42e226718706b48eb98da4f5ac3e856
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-19 23:09:22 -0300

    some improves in PlayerContext

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,43 +1,107 @@
-import {
-  createContext,
-  useState,
-  ReactNode,
-  useContext,
-  useEffect
-} from 'react'
+import { createContext, useState, ReactNode, useContext } from 'react'
 
 type PlayerContextData = {
-  song?: AlbumType
-  setSong: (song?: AlbumType) => void
+  songList: SongType[]
+  currentIndex: number
   isPlaying: boolean
+  isLooping: boolean
+  isShuffling: boolean
+  playSong: (song: SongType) => void
+  playPlayList: (list: SongType[]) => void
+  setIsPlaying: (state: boolean) => void
   togglePlay: () => void
+  toggleLoop: () => void
+  toggleShuffle: () => void
+  playNext: () => void
+  playPrevious: () => void
+  clearPlayerState: () => void
+  currentSong: SongType
+  hasNext: boolean
+  hasPrevious: boolean
 }
 
 export const PlayerContext = createContext({} as PlayerContextData)
 
-type PlayerProviderProps = {
+type PlayerContextProviderProps = {
   children: ReactNode
 }
 
-export function PlayerProvider({ children }: PlayerProviderProps) {
-  const [song, setSong] = useState<AlbumType>()
+export function PlayerProvider({ children }: PlayerContextProviderProps) {
+  const [songList, setSongList] = useState<SongType[]>([])
+  const [currentIndex, setCurrentIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isLooping, setIsLooping] = useState(false)
+  const [isShuffling, setIsShuffling] = useState(false)
 
-  useEffect(() => {
+  function playSong(song: SongType) {
+    setSongList([song])
+    setCurrentIndex(0)
     setIsPlaying(true)
-  }, [song])
+  }
+
+  function playPlayList(list: SongType[]) {
+    setSongList(list)
+    setIsPlaying(true)
+  }
 
   function togglePlay() {
     setIsPlaying(!isPlaying)
   }
 
+  function toggleLoop() {
+    setIsLooping(!isLooping)
+  }
+
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling)
+  }
+
+  function clearPlayerState() {
+    setSongList([])
+    setCurrentIndex(0)
+  }
+
+  const currentSong = songList[currentIndex]
+
+  const hasPrevious = currentIndex > 0
+  const hasNext = isShuffling || currentIndex + 1 < songList.length
+
+  function playNext() {
+    if (isShuffling) {
+      const nextRandomSongIndex = Math.floor(Math.random() * songList.length)
+
+      setCurrentIndex(nextRandomSongIndex)
+    } else if (hasNext) {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
+  function playPrevious() {
+    if (hasPrevious) {
+      setCurrentIndex(currentIndex - 1)
+    }
+  }
+
   return (
     <PlayerContext.Provider
       value={{
+        songList,
+        currentIndex,
+        playSong,
+        playPlayList,
+        playNext,
+        playPrevious,
         isPlaying,
+        isLooping,
+        isShuffling,
         togglePlay,
-        song,
-        setSong
+        setIsPlaying,
+        hasNext,
+        hasPrevious,
+        toggleLoop,
+        toggleShuffle,
+        clearPlayerState,
+        currentSong
       }}
     >
       {children}
