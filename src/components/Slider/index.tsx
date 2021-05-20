a3b55a85192b192756801553dbf9659d1b4bc6d5
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-19 23:08:26 -0300

    create Slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Slider as SliderBase,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack
+} from '@chakra-ui/slider'
+
+export type SliderProps = {
+  value?: number
+  onChange?: (value: number) => void
+}
+
+const Slider = ({ value, onChange }: SliderProps) => {
+  return (
+    <SliderBase
+      min={0}
+      max={0.999999}
+      value={value}
+      onChange={onChange}
+      step={0.001}
+    >
+      <SliderTrack>
+        <SliderFilledTrack />
+      </SliderTrack>
+      <SliderThumb />
+    </SliderBase>
+  )
+}
+
+export default Slider
