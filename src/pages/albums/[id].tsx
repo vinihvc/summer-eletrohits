a57a53c4e75934265fc6defd1e636285d4c24350
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-19 23:14:04 -0300

    improve album page, separating in new small components

--- src/pages/albums/[id].tsx
+++ src/pages/albums/[id].tsx
@@ -1,15 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 
-import { FiPlayCircle } from 'react-icons/fi'
-
-import { Box, Divider, Flex, Heading, Text } from '@chakra-ui/react'
-
-import { Image } from '@chakra-ui/image'
-import { Button } from '@chakra-ui/button'
-
-import { usePlayer } from 'contexts/PlayerContext'
+import { Box } from '@chakra-ui/react'
 
 import Base from 'templates/Base'
+
+import SongList from 'components/SongList'
+import AlbumInfo from 'components/AlbumInfo'
 
 import api from 'services/api'
 
@@ -18,60 +14,12 @@ export type AlbumsProps = {
 }
 
 function Albums({ album }: AlbumsProps) {
-  const { setSong } = usePlayer()
-
   return (
     <Base>
-      <Flex>
-        <Image src={album.thumb} borderRadius="5px" maxHeight="250px" />
-
-        <Box marginLeft="20px">
-          <Heading as="h2" size="md">
-            {album.name}
-          </Heading>
-
-          <Text mt="15px">
-            Release date:{' '}
-            {new Intl.DateTimeFormat('pt-BR').format(
-              new Date(album.releaseDate)
-            )}
-          </Text>
-
-          <Text mt="5px">Total songs: {album.songs?.length}</Text>
-
-          <Button mt="30px" onClick={() => setSong(album)}>
-            Play songs
-          </Button>
-        </Box>
-      </Flex>
+      <AlbumInfo album={album} />
 
       <Box mt="50px">
-        {album.songs?.map((song: SongType, index: number) => (
-          <Box key={song.id} my="10px">
-            <Flex mb="10px">
-              <Flex flex="6 1 0" mr="20px" fontWeight="bold">
-                {song.name}
-              </Flex>
-
-              <Flex flex="9 1 0">{song.singer}</Flex>
-
-              <Box
-                onClick={() =>
-                  setSong({
-                    thumb: '',
-                    name: `${song.name} - ${song.singer}`,
-                    videoUrl: song.videoUrl
-                  } as AlbumType)
-                }
-                _hover={{ cursor: 'pointer' }}
-              >
-                <FiPlayCircle size="20px" />
-              </Box>
-            </Flex>
-
-            {index < album.songs.length - 1 && <Divider />}
-          </Box>
-        ))}
+        <SongList songs={album.songs} />
       </Box>
     </Base>
   )
