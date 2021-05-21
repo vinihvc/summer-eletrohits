06fbe6bb72ee98b59addbd5d81d68e43fcf8ca70
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-20 23:36:46 -0300

    override body styles from chakra

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,21 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-  useSystemColorMode: false
+  useSystemColorMode: true
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  styles: {
+    global: (props) => ({
+      body: {
+        color: mode('gray.600', 'whiteAlpha.900')(props),
+        bg: mode('white', 'gray.800')(props)
+      }
+    })
+  }
+})
 
 export default theme
