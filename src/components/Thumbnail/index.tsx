74671a9d2e7a6d656df7712b6cf912bff11950d7
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-20 23:38:48 -0300

    improve Thumbnail component

--- src/components/Thumbnail/index.tsx
+++ src/components/Thumbnail/index.tsx
@@ -1,47 +1,75 @@
-import Link from 'next/link'
+import NextLink from 'next/link'
 
-import { Box, Text } from '@chakra-ui/layout'
+import { Box, Flex, Link, Text } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
+
+import { MdPlayArrow } from 'react-icons/md'
+import { useColorModeValue } from '@chakra-ui/color-mode'
 
 export type ThumbnailProps = {
   imageOnly?: boolean
   onClick?: () => void
 } & AlbumType
 
-const Thumbnail = ({
-  id,
-  thumb,
-  name,
-  imageOnly = false,
-  onClick
-}: ThumbnailProps) => (
-  <>
-    <Image
-      src={thumb}
-      alt={name}
-      objectFit="cover"
-      width="100%"
-      minHeight="100px"
-      borderRadius="3xl"
-      onClick={onClick}
-      cursor="pointer"
-      transition="0.2s"
-      _hover={{ opacity: 0.3 }}
-    />
+const Thumbnail = ({ id, thumb, name, onClick }: ThumbnailProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
 
-    {!imageOnly && (
-      <Box marginTop="10px">
-        <Link href={`/albums/${id}`}>
-          <Text
-            fontWeight="medium"
-            _hover={{ cursor: 'pointer', textDecoration: 'underline' }}
+    !!onClick && onClick()
+  }
+
+  return (
+    <Link as={NextLink} href={`/albums/${id}`}>
+      <Box
+        bg={useColorModeValue('gray.100', 'gray.900')}
+        p="15px"
+        borderRadius="lg"
+        cursor="pointer"
+        transition=".3s"
+        _hover={{
+          bg: useColorModeValue('gray.50', 'gray.700')
+        }}
+      >
+        <Box position="relative">
+          <Image
+            src={thumb}
+            alt={name}
+            objectFit="cover"
+            width="100%"
+            height="200px"
+            borderRadius="lg"
+            transition="0.2s"
+            _hover={{
+              '&+div': { opacity: 0.8 }
+            }}
+          />
+
+          <Flex
+            onClick={handleClick}
+            color="white"
+            position="absolute"
+            bottom="15px"
+            right="15px"
+            bg="black"
+            borderRadius="full"
+            cursor="pointer"
+            opacity="0"
+            transition=".3s"
+            boxSize="40px"
+            justify="center"
+            align="center"
+            _hover={{ transform: 'scale(1.2)', opacity: 1 }}
           >
-            {name}
-          </Text>
-        </Link>
+            <MdPlayArrow size="25" />
+          </Flex>
+        </Box>
+
+        <Text fontWeight="medium" marginTop="10px">
+          {name}
+        </Text>
       </Box>
-    )}
-  </>
-)
+    </Link>
+  )
+}
 
 export default Thumbnail
