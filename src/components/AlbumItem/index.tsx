619012d0c183dfc4f0fc66d28dd50dceff312915
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-20 23:35:16 -0300

    create AlbumItem component

---
+++ src/components/AlbumItem/index.tsx
@@ -0,0 +1,15 @@
+import { usePlayer } from 'contexts/PlayerContext'
+
+import Thumbnail from 'components/Thumbnail'
+
+export type AlbumItem = {
+  album: AlbumType
+}
+
+const AlbumItem = ({ album }: AlbumItem) => {
+  const { playPlayList } = usePlayer()
+
+  return <Thumbnail {...album} onClick={() => playPlayList(album.songs!)} />
+}
+
+export default AlbumItem
