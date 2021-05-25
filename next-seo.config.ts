ef168b185025ededcb89bf411c7d265c6e2e45de
Vinicius Vicentini <viniciusvicentini@live.com>
2021-05-24 23:02:30 -0300

    add next-seo to project

---
+++ next-seo.config.ts
@@ -0,0 +1,23 @@
+export default {
+  titleTemplate: 'Summer Eletrohits - %s',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'http://summer-eletrohits-vercel.app/',
+    site_name: 'Summer Eletrohits',
+    title: 'Summer Eletrohits',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/viniciushvc/image/upload/v1621562469/summer-eletrohits/12_icx47c.jpg',
+        width: 300,
+        height: 300,
+        alt: 'Summer Eletrohits'
+      }
+    ]
+  },
+  twitter: {
+    handle: 'http://summer-eletrohits-vercel.app/',
+    site: 'http',
+    cardType: 'summary_large_image'
+  }
+}
