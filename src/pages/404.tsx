84e848291ee281bb2acd349e8226e16d0a49b237
Vinicius Vicentini <viniciusvicentini@live.com>
2022-04-16 18:51:52 -0300

    feat: not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+import { Button, Flex, Stack, Heading } from '@chakra-ui/react'
+
+const NotFoundPage = () => {
+  return (
+    <Flex align="center" justify="center" h="calc(100vh - 300px)">
+      <Stack textAlign="center" spacing={8}>
+        <Stack spacing={0}>
+          <Heading fontSize="3xl" fontWeight="bold">
+            Page Not Found
+          </Heading>
+        </Stack>
+
+        <Link href="/" passHref>
+          <Button>Back to Home</Button>
+        </Link>
+      </Stack>
+    </Flex>
+  )
+}
+
+export default NotFoundPage
