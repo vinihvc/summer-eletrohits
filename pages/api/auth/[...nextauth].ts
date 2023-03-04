28a7f796ff97dc23c9eb46eac06fe1bc4f51b6b3
Vinicius Vicentini <viniciusvicentini@live.com>
2023-03-04 12:00:50 -0300

    update preview on vercel

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,36 @@
 import { db } from '@/libs/db'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
+    process.env.VERCEL_ENV === 'preview'
+      ? CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            username: {
+              label: 'Username',
+              type: 'text',
+              placeholder: 'vinicius',
+            },
+            password: { label: 'Password', type: 'password' },
+          },
+          async authorize() {
+            return {
+              id: '1',
+              name: 'V Vicentini',
+              email: 'vinicius@example.com',
+              image: 'https://i.pravatar.cc/150?u=vinicius@example.com',
+            }
+          },
+        })
+      : GoogleProvider({
+          clientId: process.env.GOOGLE_ID || '',
+          clientSecret: process.env.GOOGLE_SECRET || '',
+        }),
   ],
 }
 
