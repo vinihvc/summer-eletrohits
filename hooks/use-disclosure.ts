b36a80d9820bb155f4c8278e3ec6086d0e239b47
Vinicius Vicentini <viniciusvicentini@live.com>
2023-03-21 14:40:02 -0300

    update CI

--- hooks/use-disclosure.ts
+++ hooks/use-disclosure.ts
@@ -4,6 +4,14 @@ export type UseDisclosureProps = {
   defaultIsOpen?: boolean
 }
 
+/**
+ * Custom hook used to help handle common open, close, or toggle scenarios.
+ *
+ * @example
+ * ```js
+ * const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
+ * ```
+ */
 export const useDisclosure = (props: UseDisclosureProps = {}) => {
   const [isOpenState, setIsOpen] = useState(props.defaultIsOpen || false)
 
