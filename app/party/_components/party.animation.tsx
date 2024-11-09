071fe836b34e9a7575578ce48aff5bce5b4ae032
Vinicius Vicentini <viniciusvicentini@live.com>
2024-11-09 20:08:32 -0300

    fix build

--- app/party/_components/party.animation.tsx
+++ app/party/_components/party.animation.tsx
@@ -1,9 +1,6 @@
 import LottiePlayer from '@/components/ui/lottie'
-import { usePartyStore } from '../party.store'
 
 export const PartyAnimation = () => {
-  const { isAnimating } = usePartyStore()
-
   return (
     <div className="relative aspect-video w-[450px]">
       <LottiePlayer path="/lottie/parrot.lottie" />
